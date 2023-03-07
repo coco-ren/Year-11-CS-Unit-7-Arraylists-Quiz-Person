493607e0c745bead4b236a00ea989d949415584f
Coco <kecr2025@student.cis.edu.hk>
2023-03-07 12:45:01 +0800

    done

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,42 @@ public class Person {
     private String lastName;
     private int age;
 
+//constructor
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+//getters
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+//setters
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+//toString
+    public String toString() {
+        return "First Name = " + firstName + ", " + "Last Name = " + lastName + ", " + "Age = " + age + ".";
+    }
 }
+
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -9,14 +9,37 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
-
+        if (allPersons.size() == 0) {
+            allPersons.add(0, person);
+        } else {
+            int pos = 0;
+            for (int i = 0; i < allPersons.size(); i++) {
+                Person onePerson = allPersons.get(i);
+                if (person.getLastName().compareTo(onePerson.getLastName()) <= 0) {
+                    pos = i;
+                    break;
+                }
+            }
+            allPersons.add(pos,person);
+        }
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> output = new ArrayList<>();
+        for (Person person : allPersons) {
+            if (person.getAge() < 18) {
+                output.add(person);
+            }
+        }
+        return output;
     }
 
     public boolean checkLastName(String lastName) {
-
+        for (Person person : allPersons) {
+            if (person.getLastName().equals(lastName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
